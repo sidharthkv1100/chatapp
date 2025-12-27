e64e7068f3edc8d3b0fb3c9a53a32baeb5cd0468
Sidharth K V <sidharthkv1100@gmail.com>
2025-12-27 17:35:53 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyCxrPGYp_EYeYtWWPEP34Utw5USMKPJTNY",
+  authDomain: "react-chat-app-c92a8.firebaseapp.com",
+  projectId: "react-chat-app-c92a8",
+  storageBucket: "react-chat-app-c92a8.appspot.com",
+  messagingSenderId: "864073835846",
+  appId: "1:864073835846:web:7daed5778d9602ed56b147",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
